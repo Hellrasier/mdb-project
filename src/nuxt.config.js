7afb826eba0c77317f5bc3dbbf58edde8ab0d3c2
Hellrasier <denislolipop48@gmail.com>
2022-06-12 20:55:12 +0300

    Made global stylesheet

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -2,5 +2,34 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
+  
+  buildModules: [
+    '@nuxtjs/tailwindcss',
+  ],
+  
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+      },
+    },
+  },
+  
+  css: [
+    '~/assets/css/main.sass',
+    '~/assets/fonts/stylesheet.css'
+  ],
+  
+  // vite: {
+  //     css: {
+  //         preprocessorOptions: {
+  //             sass: {
+  //                 additionalData: '@import "@/assets/css/main.sass"',
+  //             },
+  //         },
+  //     },
+  // }
 })
