37aa97362eeb3cbcbb18be7fcdcfdd3ffd2e439d
Yarosls <moyodoym@gmail.com>
2022-06-13 03:07:02 +0300

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,121 @@
 <template>
-  <div>
+
+  <div class="menu_buttoms">
+    <p  class="logo">
+       <img src="/images/logo.png" alt="logo"  />
+    </p>
+    <div class="one">Фильмы</div>
+    <div class="two">Сериалы</div>
+    <div class="three">Мультфильмы</div>
+    <div class="four">Аниме</div>
+    <div><span class="megasearch">Мегапоиск</span></div>
+  </div>
+
+  <div class="border_bigname">
     <p class="bigname"> FILMEX </p>
   </div>
+
+    
 </template>
 
 <style lang="sass">
 
   .bigname 
     color: white
-    font-size: 290px
-    text-align: center
-    letter-spacing: 0.17em 
-    font-weight: 700
-    font-height: 354
+    font-size: 19em      
+    letter-spacing: 0.25em
+      
+
+    
+    
+
+
+  .border_bigname
+    display: flex  
+    justify-content: center
+    align-items: center   
+    height: 15em
+    border: medium dashed green
+
+    
+    
+
+
+
+  .menu_buttoms
+    display: flex
+    margin-top: 1em
+    margin-left: 5em
+    font-size: 90%
+    
+
+ 
+   
+
+  .logo
+    flex: 0.05 
+    
+
+  .one
+    font-size: 14px
+    flex: 0.09 
+    border: 1px solid white
+    position: relative
+    height: 2.5em
+    text-align: center 
+    margin: 0.4em 1em 1em 0em
+    padding: 0.4em 0em 1em 0em
+    
+    
+    
+    
+
+  .two
+    font-size: 14px
+    flex: 0.09 
+    border: 1px solid white
+    position: relative
+    height: 2.5em
+    text-align: center 
+    margin: 0.4em 1em 1em 0em
+    padding: 0.4em 0em 1em 0em
+
+    
+
+  .three
+    font-size: 14px
+    flex: 0.11 
+    border: 1px solid white
+    position: relative
+    height: 2.5em
+    text-align: center 
+    margin: 0.4em 1em 1em 0em
+    padding: 0.4em 0em 1em 0em
+
+  .four 
+    font-size: 14px
+    flex: 0.1 
+    border: 1px solid white
+    position: relative
+    height: 2.5em
+    text-align: center 
+    margin: 0.4em 1em 1em 0em
+    padding: 0.4em 0em 1em 0em
+
+
+  .megasearch
+    font-size: 15px
+    position: relative
+    right: -45em
+    border: 1px solid white
+    position: relative
+    text-align: center 
+  
+  
+      
+
+    
+
 
 
 
