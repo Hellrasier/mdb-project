4a3b2de785f65265a452e3f73a85fac2e1f14afd
Yarosls <moyodoym@gmail.com>
2022-06-13 13:13:07 +0300

    dorabotka_header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,7 @@
 <template>
-
   <div class="menu_buttoms">
-    <p  class="logo">
-       <img src="/images/logo.png" alt="logo"  />
+    <p class="logo">
+      <img src="/images/logo.png" alt="logo" />
     </p>
     <div class="one">Фильмы</div>
     <div class="two">Сериалы</div>
@@ -12,114 +11,83 @@
   </div>
 
   <div class="border_bigname">
-    <p class="bigname"> FILMEX </p>
+    <p class="bigname">FILMEX</p>
   </div>
-
-    
 </template>
 
 <style lang="sass">
-
-  .bigname 
-    color: white
-    font-size: 19em      
-    letter-spacing: 0.25em
-      
-
-    
-    
-
-
-  .border_bigname
-    display: flex  
-    justify-content: center
-    align-items: center   
-    height: 15em
-    border: medium dashed green
-
-    
-    
-
-
-
-  .menu_buttoms
-    display: flex
-    margin-top: 1em
-    margin-left: 5em
-    font-size: 90%
-    
+.bigname
+  color: white
+  font-size: 20em
+  letter-spacing: 0.30em
+  
 
  
-   
-
-  .logo
-    flex: 0.05 
-    
-
-  .one
-    font-size: 14px
-    flex: 0.09 
-    border: 1px solid white
-    position: relative
-    height: 2.5em
-    text-align: center 
-    margin: 0.4em 1em 1em 0em
-    padding: 0.4em 0em 1em 0em
-    
-    
-    
-    
-
-  .two
-    font-size: 14px
-    flex: 0.09 
-    border: 1px solid white
-    position: relative
-    height: 2.5em
-    text-align: center 
-    margin: 0.4em 1em 1em 0em
-    padding: 0.4em 0em 1em 0em
-
-    
-
-  .three
-    font-size: 14px
-    flex: 0.11 
-    border: 1px solid white
-    position: relative
-    height: 2.5em
-    text-align: center 
-    margin: 0.4em 1em 1em 0em
-    padding: 0.4em 0em 1em 0em
-
-  .four 
-    font-size: 14px
-    flex: 0.1 
-    border: 1px solid white
-    position: relative
-    height: 2.5em
-    text-align: center 
-    margin: 0.4em 1em 1em 0em
-    padding: 0.4em 0em 1em 0em
+.border_bigname
+  display: flex 
+  justify-content: center 
+  align-items: center 
+  height: 10em
+  //border: medium dashed green
+  padding:  5em 5em 0em 10em
 
 
-  .megasearch
-    font-size: 15px
-    position: relative
-    right: -45em
-    border: 1px solid white
-    position: relative
-    text-align: center 
+.menu_buttoms
+  display: flex 
+  margin-top: 1em  
+  margin-left: -1em 
+  font-size: 90% 
+
+
+.logo
+  flex: 0.05 
+
+.one
+  font-size: 14px 
+  flex: 0.09 
+  border: 1px solid white 
+  position: relative 
+  height: 2.5em 
+  text-align: center 
+  margin: 0.4em 1em 1em 0em 
+  padding: 0.4em 0em 1em 0em 
+
+.two
+  font-size: 14px 
+  flex: 0.09  
+  border: 1px solid white 
+  position: relative
+  height: 2.5em 
+  text-align: center 
+  margin: 0.4em 1em 1em 0em 
+  padding: 0.4em 0em 1em 0em 
+
+.three
+  font-size: 14px
+  flex: 0.11
+  border: 1px solid white
+  position: relative
+  height: 2.5em
+  text-align: center
+  margin: 0.4em 1em 1em 0em
+  padding: 0.4em 0em 1em 0em
+
+.four
+  font-size: 14px
+  flex: 0.1
+  border: 1px solid white
+  position: relative
+  height: 2.5em
+  text-align: center
+  margin: 0.4em 1em 1em 0em
+  padding: 0.4em 0em 1em 0em
+
+.megasearch
+  font-size: 14px
+  border: 1px solid white
   
+
   
-      
-
-    
-
-
-
-
-
 </style>
 
 <!-- level 1 -->
