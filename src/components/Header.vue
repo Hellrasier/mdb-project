3f0f7086336f4fdc811a253f9abe314cde747d71
Yarosls <moyodoym@gmail.com>
2022-06-19 13:10:36 +0300

    header_3

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,26 @@
 <template>
 <div class="header">
-  <div class="menu_buttons">
     <p class="logo">
       <img src="/images/logo.svg" alt="logo" />
     </p>
-    <div class="one">
-      <input type="button" /> Фильмы
-    </div>
+ 
+
+  <div class="menu_buttons">
+    <div class="one">Фильмы</div>
     <div class="two">Сериалы</div>
     <div class="three">Мультфильмы</div>
     <div class="four">Аниме</div>
   </div>
-    <div class="between"><span class="megasearch">МЕГАПОИСК</span></div>
-  </div>
-
-
+<div class="between"><span class="megasearch">МЕГАПОИСК</span></div>
+    
+</div>
   <div class="border_bigname">
-    <span class="bigname">FILMEX</span>
+    <span class="bigname first">F</span>
+    <span class="bigname">I</span>
+    <span class="bigname">L</span>
+    <span class="bigname">M</span>
+    <span class="bigname">E</span>
+    <span class="bigname">X</span>
 </div>
 
 </template>
@@ -32,97 +36,89 @@
 .bigname
   color: white
   font-family: HelveticaNeueCyr
-  font-size: 37vh
+  font-size: 18vw
   font-style: normal
   font-weight: 700
-  text-align: center
-  font-stretch: 200% 
-  letter-spacing: 0.15em
-  line-height: 1em
-  
-  
+  //letter-spacing: 0.2em
+  line-height: 0.9em
+     
 
  
 .border_bigname
-  display: flex 
-  justify-content: center 
-  align-items: center 
-   //border: medium dashed green
+  display: flex  
+  text-align: center
+  justify-content: space-between
+    
+.first
+  margin-left: -1.2vw
+
+
+  //border: medium dashed green
  
 
 .header
   display: flex
   justify-content: space-between
+  max-width: 170px
 
 
 .menu_buttons
-  display: flex 
+  display: flex  
+  align-items: center
   flex-direction: row
   height: 100%
-  width: 90%
-  font-weight: 400 
-  margin: 0.4em 1em 1em 0em
+  width: 30%
+  gap: 2em
+  div
+    display: flex 
+    justify-content: center 
+    align-items: center 
+    font-size: 13px 
+    flex-basis: 20%
+    border: 1px solid white 
+    padding: 0.3em 0.5em 
+    padding-right: 15px
+    padding-left: 15px
+  
  
+   
+     
+.img
+
   
 
 .logo
-  flex: 0.05 
-
-.one
-  font-size: 14px 
-  flex: 0.09 
-  border: 1px solid white 
-  position: relative
-  height: 2.5em 
-  text-align: center 
-  margin: 0.4em 1em 1em 0em 
+  display: flex  
+  align-items: center
+  justify-content: space-between
   
 
-.two
-  font-size: 14px 
-  flex: 0.09  
-  border: 1px solid white 
-  position: relative
-  height: 2.5em 
-  text-align: center 
-  margin: 0.4em 1em 1em 0em 
-   
 
-.three
-  font-size: 14px
-  flex: 0.11
-  border: 1px solid white
-  position: relative
-  height: 2.5em
-  text-align: center
-  margin: 0.4em 1em 1em 0em
- 
 
-.four
-  font-size: 14px
-  flex: 0.1
-  border: 1px solid white
-  position: relative
-  height: 2.5em
-  text-align: center
-  margin: 0.4em 1em 1em 0em
- 
-//.between
-  
 
 .between
-  font-size: 14px
+  display: flex 
+  justify-content: center 
+  display: flex 
+  font-size: 13px
+  font-family: HelveticaNeueCyr
+  font-style: bold
   border: 2px solid red
   border-radius: 25em   
   letter-spacing: 0.1em
   text-align: center 
   font-stretch: 200% 
-  width: 20em
+  width: 14em
+  height: 2.2em
   position: relative
+  align-items: center
   
 
   
+  
 
+.megasearch
+  
 
 
 
